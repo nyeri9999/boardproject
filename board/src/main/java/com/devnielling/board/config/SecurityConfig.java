ad6b48d53f00ee69ea6ed7137cfeb79c4682a2ed
niell0 <neoyeo66@gmail.com>
2025-10-15 18:45:42 +0900

    first upload

---
+++ board/src/main/java/com/devnielling/board/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.devnielling.board.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable());
+
+        http
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll());
+
+        http
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
